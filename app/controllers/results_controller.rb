4c03340f26d2fa0064a2c1d72d028eb1b184c259
KJAVA19811815 <karunsingh1993@gmail.com>
2017-06-22 16:00:48 +0000

    working on joins for AR queries

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,54 +2,33 @@ class ResultsController < ApplicationController
 
   def index
     @user = User.joins(:answers, answers:[{choice: :question}, :choice])
-    puts @user.inspect, 'USER JOIN'
+    #puts @user.inspect, 'USER JOIN'
     #@test = [1,2,3,4,5,6]
   end
 
   def example
-    @user = User.find_by(sex: 'female')
-    #@answer = @user.answers.map { |e| e.choice  }
-    #@question = @answer.map { |e| e.question  }
-    #@form = @question.map { |e| e.form  }
+    #@arr = []
+    #@users = User.where(sex: 'male').find_each do |u|
+       #@answers = u.answers
+       #@choices = @answers.map { |e| e.choice }
+       #@questions = @choices.map { |e| e.question }
+       #$@forms = @questions.map { |e| e.form }
 
-    #@users = User.all
-    #@answer = @users.map { |e| e.answers  }
-    #@choice = @answer.map {  |e| e.choices  }
-    # @new_items = []
-    # items = [1,2,3,4]
-    # items.map do |item|
-    #   @new_item << { original: item, double: (2 * item) }
-    # end
-    @arr = []
-    @users = User.where(sex: 'male').find_each do |u|
-       @answers = u.answers
-       @choices = @answers.map { |e| e.choice }
-       @questions = @choices.map { |e| e.question }
-       @forms = @questions.map { |e| e.form }
-
-       @users.map do |item|
-         @arr << {item: item}
-       end
+      #  sql = "Select * from ... your sql query here"
+      #  records_array = ActiveRecord::Base.connection.execute(sql)
 
        #binding.pry
+       @users = User.where(sex: 'female')
+       @choices = Choice.joins(answers: [:user]).where(users: {sex: 'female'}, choices_name: 'NO')
+       @answers = Answer.includes(:choice).joins(:user).where(users: {sex: 'female'})
 
-
-    end
+#end
 
     result = {
-      users: @users,
-      #answer: @answer,
-      #choice: @choice
-      user: @user,
-      answers: @answers,
       choices: @choices,
-      questions: @questions,
-      forms: @forms
-      #answer: @answer,
-      #question: @question,
-      #form: @form
+      users: @users,
+      answers: @answers
     }
     render json: result
   end
-
 end
